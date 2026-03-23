1d3bb8171bd34190cfc136020c1710beeaaba4ad
2568832 <2568832@cegepmontpetit.ca>
2026-03-23 14:05:34 -0400

    Console Exercices fini

---
+++ Intro aux exceptions en console/Intro aux exceptions en console/CompteBancaire.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_aux_exceptions_en_console
+{
+    public class CompteBancaire
+    {
+        
+            public double Solde { get; private set; }
+
+            public CompteBancaire()
+            {
+                Solde = 100.0;
+            }
+
+            public void Retirer(double montant)
+            {
+                
+            if (montant > Solde)
+                {
+                    throw new InvalidOperationException(
+                        $"Impossible de retirer {montant}$ : solde insuffisant ({Solde}$)."
+                    );
+                }
+
+                Solde -= montant;
+            }
+        
+
+    }
+}
--- Intro aux exceptions en console/Intro aux exceptions en console/Program.cs
+++ Intro aux exceptions en console/Intro aux exceptions en console/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,57 @@ namespace Intro_aux_exceptions_en_console
     {
         static void Main(string[] args)
         {
+            //EX1
+            try
+            {
+                int x = int.Parse("bonjour");
+                Console.WriteLine(x);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Écheque mot");
+            }
+
+            // Ex3
+
+            CompteBancaire compteBancaire = new CompteBancaire();
+
+            try
+            {
+                compteBancaire.Retirer(150);
+            }
+            catch (InvalidOperationException a)
+            {
+                Console.WriteLine(a.Message);
+            }
+
+            //EX4
+
+            try
+            {
+                using (StreamReader sr = File.OpenText("data.txt"))
+                {
+                    Console.WriteLine($"La première ligne du fichier est : {sr.ReadLine()}");
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("fichier introuvable.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("dossier introuvable.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Erreur IOException");
+                Console.WriteLine(ex.Message);
+            }
         }
+
+
     }
 }
+
